2c0b835cc44cd9f21ed677e2458d2d88bc667221
Liopol2 <leonardosoumas3@gmail.com>
2024-06-25 19:40:32 -0300

    Batman

---
+++ installer.ps1
@@ -0,0 +1,16 @@
+# Start a new PowerShell process to execute install.ps1
+try {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+catch {
+    Write-Error "No se pudo instalar chocolatey ggez error:  $_.Exception.Message"
+}
+try {
+    # Start a new PowerShell process to execute programs.ps1
+    Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    Start-Process powershell -Verb RunAs -ArgumentList "-File `".\programs.ps1`""  
+    Write-Host "instalando programas"
+}
+catch {
+    Write-Error "Los progrmas no programan. ggez error:  $_.Exception.Message"
+}
